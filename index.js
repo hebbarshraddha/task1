5683901de28ea5f4703ba79f8a9da4c312f44ae9
hebbarshraddha <shraddha.21ad050@sode-edu.in>
2023-11-19 21:50:19 +0530

    firstintern

---
+++ index.js
@@ -0,0 +1,62 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+
+var mongoose=require("mongoose")
+
+var app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+
+
+mongoose.connect('mongodb+srv://shrad30:15367322X@cluster0.tzy67ux.mongodb.net/?retryWrites=true&w=majority',).then((response)=>{console.log('Connected to database');}) .catch((error)=>{console.log(error);});
+
+
+
+//mongoose.connect('mongodb+srv://shrad30:15367322X@cluster0.tzy67ux.mongodb.net/personal-task-manager?retryWrites=true&w=majority',).then((response)=>
+  //              {console.log("Connected to the database");}).catch((error)=>{console.log(error);}); 
+//mongoose.connect('mongodb+srv://shrad30:15367322X@cluster0.tzy67ux.mongodb.net/personal-task-manager?retryWrites=true&w=majority',{
+    //useNewUrlParser:true,
+    //useUnifiedTopology:true
+//});
+var db=mongoose.connection;
+
+db.on("error",()=>console.log("error in database"));
+db.once('open',()=>console.log("connected to database"));
+app.post('/register',(req,res)=>{
+    var name=req.body.name;
+    var email=req.body.email;
+    var phnum=req.body.phnum;
+    var password=req.body.password;
+
+    var data={
+        "name":name,
+        "email":email,
+        "phnum":phnum,
+        "password":password,
+        
+    }
+    db.collection('users').insertOne(data,(err,collection)=>{
+       if(err){
+        throw err;
+       }
+       console.log("Record Inserted Successfully");
+            
+        });
+        return res.redirect("signup_sucess.html")
+    })
+
+
+
+
+app.get("/",(req,res)=>{
+   // res.send("hello from server")
+   res.set({"Allow-access-Allow-Origin":'*'})
+   return res.redirect("index.html");
+}).listen(3000);
+
+console.log("Listening on port 3000")
